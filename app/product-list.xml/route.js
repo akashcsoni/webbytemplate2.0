1d9db5cf23cc6421acb6ce3c8e1b379f63c97c72
webbycrown8 <programmer8@webbycrown.com>
2025-10-28 12:49:33 +0530

    SEO Changes Add

--- app/product-list.xml/route.js
+++ app/product-list.xml/route.js
@@ -48,6 +48,7 @@ ${sitemapEntries.map(entry => `  <url>
             status: 200,
             headers: {
                 'Content-Type': 'application/xml',
+                'X-Robots-Tag': 'noindex, nofollow', // Prevent indexing of sitemap XML files
                 'Cache-Control': 'public, max-age=3600, s-maxage=3600', // Cache for 1 hour
             },
         });
